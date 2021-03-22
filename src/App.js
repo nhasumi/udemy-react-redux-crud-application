dc5176599e4a5c0fc8d7d56e25e116877e5b7694
nhasumi <nhasumi01@gmail.com>
2021-03-22 12:06:22 +0900

    変更２

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          wellcom React
+          wellcom React2
         </a>
       </header>
     </div>
