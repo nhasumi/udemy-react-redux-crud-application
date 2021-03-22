df5816727a128c26796647956fd18ed396df533d
nhasumi <nhasumi01@gmail.com>
2021-03-22 15:12:52 +0900

    components create

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+/*
 class App extends Component {
   render(){
     let greeting="Hi.Test";
@@ -7,8 +8,14 @@ class App extends Component {
     <div>
       <input type="text" onChange={()=> {console.log("I am clicked")}}/>
       <h1> {greeting} hellow world!</h1>
+
     </div>
       );
   }
 }
+*/
+const App = () => {
+  return (<div>Hi!</div>)
+}
+
 export default App;
