bb504f508968419998b364e73403ff25353cf3b3
nhasumi <nhasumi01@gmail.com>
2021-03-22 14:44:31 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, { Component } from 'react';
 
-
-function App() {
-  return (
+class App extends Component {
+  render(){
+    let greeting="Hi.Test";
+    return (
     <div>
-         <h1> wellcom React3</h1>
+      <input type="text" onChange={()=> {console.log("I am clicked")}}/>
+      <h1> {greeting} hellow world!</h1>
     </div>
-  );
+      );
+  }
 }
-
 export default App;
