388e59acd0b2aaeb107658f53fca70a3d0f4afb2
nhasumi <nhasumi01@gmail.com>
2021-03-22 12:45:32 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" adfa/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          wellcom React3
-        </a>
-      </header>
+    <div>
+         <h1> wellcom React3</h1>
     </div>
   );
 }
